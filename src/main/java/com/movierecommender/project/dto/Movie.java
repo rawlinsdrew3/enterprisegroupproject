4cf18e631df0f042d5ee43e13c9b2621cd361047
Ethan <Beached@mail.uc.edu>
2024-10-12 16:20:08 -0400

    Added Initial Files

---
+++ src/main/java/com/movierecommender/project/dto/Movie.java
@@ -0,0 +1,12 @@
+package com.movierecommender.project.dto;
+
+public class Movie
+{
+    public int movieId;
+    public String title;
+    public String genre;
+    public String director;
+    public int rating;
+    public String releaseYear;
+    public String description;
+}
