c18039d6e47265ec41a62b3f3d8f58ddb94ff853
markonisiama <nisiamma@mail.uc.edu>
2024-12-02 15:37:59 -0500

    Browse functionality filters is fixed and it works with the new data division as well.

--- src/main/java/com/movierecommender/project/BrowseController.java
+++ src/main/java/com/movierecommender/project/BrowseController.java
@@ -1,9 +1,4 @@
 package com.movierecommender.project;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.movierecommender.project.dto.Movie;
 import com.movierecommender.project.service.IBrowseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.io.IOException;
-//import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 public class BrowseController {
--- src/main/java/com/movierecommender/project/MovieController.java
+++ src/main/java/com/movierecommender/project/MovieController.java
@@ -1,19 +1,12 @@
 package com.movierecommender.project;
-
 import com.movierecommender.project.dto.Movie;
 import com.movierecommender.project.service.IMovieService;
-import com.movierecommender.project.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.io.IOException;
 
 
 
