4a704c701675d03357fb71cd43cf2447b58ed14a
Ethan <Beached@mail.uc.edu>
2024-12-04 18:32:21 -0500

    Added Javadoc for BrowseService, IBrowseService, and MovieService

--- src/main/java/com/movierecommender/project/service/MovieService.java
+++ src/main/java/com/movierecommender/project/service/MovieService.java
@@ -23,6 +23,15 @@ public class MovieService implements IMovieService {
     private static final String API_URL = "https://api.themoviedb.org/3/movie/";
     private static final String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmZmYTM2NjgyYTY5ZDM1OWQ1MjQ4OTFkNDQ1OGI2NiIsIm5iZiI6MTcyOTYyNDQ5MS43MjE2MTgsInN1YiI6IjY3MTdmODBjNmU0MjEwNzgwZjc4NzRlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ubnKxHe4mfL51OE8NhOz31gacsvQU0wyh1Ja6vfp4D0"; // Replace with your actual token
 
+    /**
+     * Fetches a movie from an external API using the movie ID.
+     * The movie data is returned as a movie object.
+     *
+     * @param movieId The unique identifier of the movie to fetch.
+     * @return A movie object containing the movie details.
+     * @throws IOException If there is an issue with the API request or response.
+     * @throws InterruptedException If the API request is interrupted.
+     */
     @Override
     public Movie fetchMovieFromExternalApi(int movieId) throws IOException, InterruptedException {
         String url = API_URL + movieId + "?language=en-US";
@@ -44,11 +53,23 @@ public class MovieService implements IMovieService {
         }
     }
 
+    /**
+     * Retrieves all movies stored in the repository.
+     *
+     * @return A collection of all movie objects in the repository.
+     */
     @Override
     public Iterable<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
+    /**
+     * Saves a movie in the repository.
+     * If the movie already exists, it will update the existing record.
+     *
+     * @param movie The movie object to save.
+     * @return The saved movie object.
+     */
     @Override
     public Movie saveMovie(Movie movie) {
         return movieRepository.save(movie);
